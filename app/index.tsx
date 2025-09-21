50dc5092303c2346d88736aa697ceaa277eca071
fouz0062 <fouzifathii@gmail.com>
2025-09-21 20:24:46 +0200

    designed the homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,26 @@
 import profile from '@/assets/images/profile.jpg';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageWrapper}>
         <Image source={profile} style={styles.profile} />
       </View>
       <Text style={styles.title}>Mohamed Ashik</Text>
+      <TouchableOpacity style={styles.btn}><Text style={styles.btnTxt}>About</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.btn}><Text style={styles.btnTxt}>Skills</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.btn}><Text style={styles.btnTxt}>Experience</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.btn}><Text style={styles.btnTxt}>Contact</Text></TouchableOpacity>
     </View>
   );
 }
@@ -27,7 +40,7 @@ const styles = StyleSheet.create({
     borderColor: '#1a2b4c',
     borderWidth: 2,
     elevation: 8,
-    backgroundColor: '#000', 
+    backgroundColor: '#fff',
     overflow: 'hidden',
   },
   profile: {
@@ -36,10 +49,25 @@ const styles = StyleSheet.create({
     borderRadius: 100,
   },
   title: {
-    display: 'flex',
     textAlign: 'center',
-    fontSize: 30,
-    marginTop: 5,
-    fontWeight:'bold'
+    fontSize: 28,
+    marginTop: 10,
+    fontFamily: 'Poppins_700Bold',
+  },
+  btn: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#1a2b4c',
+    marginVertical: 5,
+    width: 100,
+    marginHorizontal: 'auto',
+    borderRadius:10
+  },
+    btnTxt: {
+      color: '#f8f8ff',
+      fontSize: 15,
+      paddingVertical:2
   }
 });

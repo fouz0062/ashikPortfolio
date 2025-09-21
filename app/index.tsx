dc8b125010f461aee4a6fff54d94851c5eff6ab9
fouz0062 <fouzifathii@gmail.com>
2025-09-21 20:14:40 +0200

    installed google fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,45 @@
-import { Text, View } from "react-native";
+import profile from '@/assets/images/profile.jpg';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.imageWrapper}>
+        <Image source={profile} style={styles.profile} />
+      </View>
+      <Text style={styles.title}>Mohamed Ashik</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f8ff',
+    padding: 20,
+  },
+  imageWrapper: {
+    alignSelf: 'center',
+    marginTop: 50,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    borderColor: '#1a2b4c',
+    borderWidth: 2,
+    elevation: 8,
+    backgroundColor: '#000', 
+    overflow: 'hidden',
+  },
+  profile: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 100,
+  },
+  title: {
+    display: 'flex',
+    textAlign: 'center',
+    fontSize: 30,
+    marginTop: 5,
+    fontWeight:'bold'
+  }
+});
